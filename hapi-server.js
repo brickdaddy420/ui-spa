fb179f650b498ef124e686ec9528a1281e707333
brickdaddy420 <71885167+brickdaddy420@users.noreply.github.com>
2020-12-04 10:38:03 -0500

    e
    e

--- hapi-server.js
+++ hapi-server.js
@@ -2,10 +2,10 @@
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "localhost", // PostgreSQL server
-    user: "tom", // Your user name
-    password: "", // Your password
-    database: "cos-243-ui-spa", // Your database name
+    host: 'faraday.cse.taylor.edu', // PostgreSQL server
+    user: 'brickson_cain', // Your user name
+    password: 'timogaha', // Your password
+    database: 'brickson_cain', // Your database name
   },
 });
 
